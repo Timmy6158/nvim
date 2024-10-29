a195a44a4c3d7fbb48d8ea44308f8ce9e769f0d9
Timmy6158 <roomshocker@gmail.com>
2024-10-28 18:43:14 -0700

    Made git repo the only nvim projects

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -5,13 +5,27 @@ return {
     config = function()
       require("project_nvim").setup({
         -- Project root detection methods
-        detection_methods = { "lsp", "pattern" },
+        detection_methods = { "pattern", "lsp" },
 
         -- Patterns to detect project root
-        patterns = { ".git", "Makefile", "package.json" },
+        patterns = { ".git" },
+
+        -- Search parent directory
+        search_inside_files = false,
 
         -- Excluded directories
-        exclude_dirs = { "node_modules", "vendor", ".git" },
+        exclude_dirs = {
+          "node_modules",
+          "__pycache__",
+          ".pytest_cache",
+          "*.egg-info",
+          ".venv",
+          "env",
+          "venv",
+        },
+
+        -- This tells project.nvim to always search upwards for patterns
+        scope_chdir = 'global',
 
         -- Recent projects
         show_recent_projects = true,
